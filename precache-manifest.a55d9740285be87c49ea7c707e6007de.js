81c77352de44b7e41b929f8bd5ade782296b058d
Anupam Sharma <anupamsharma221@yahoo.com>
2022-11-13 22:13:14 +0530

    Updates

---
+++ precache-manifest.a55d9740285be87c49ea7c707e6007de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7cffcc531750861e355",
+    "url": "/React_Learning/static/css/main.011644ce.chunk.css"
+  },
+  {
+    "revision": "a7cffcc531750861e355",
+    "url": "/React_Learning/static/js/main.a7cffcc5.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/React_Learning/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/React_Learning/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "fb938b48991a19295f75",
+    "url": "/React_Learning/static/js/runtime~main.fb938b48.js"
+  },
+  {
+    "revision": "b4b502c9a0e0bd71b78fbd53e36bf40d",
+    "url": "/React_Learning/index.html"
+  }
+];
